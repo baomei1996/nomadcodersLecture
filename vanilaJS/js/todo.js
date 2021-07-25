22e3cb802c385ba2f37e93b7ed85710201b5998e
BomiAn <qhal4686@gmail.com>
2021-07-25 23:45:52 +0900

    nomadcodersLecture - vanilaJS

--- vanilaJS/js/todo.js
+++ vanilaJS/js/todo.js
@@ -2,10 +2,52 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input')
 const toDoList = document.getElementById('todo-list');
 
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  
+}
+
+function deleteToDo (event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo (newToDo) {
+  const li = document.createElement('li');
+  const span = document.createElement('span')
+  span.innerText = newToDo;
+  const button = document.createElement('span');
+  button.innerText = '❌';
+  button.addEventListener("click", deleteToDo)
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li)
+}
+
+function sayHello(event) {
+  console.log("this is turn of ", event)
+}
+
 function handelToDoSubmit (event) {
   event.preventDefault();
-  console.log(toDoInput.value)
-
+  const newToDo = toDoInput.value;
+  toDoInput.value = '';
+  toDos.push(newToDo)
+  paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handelToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+console.log(savedToDos);
+
+if(savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos)
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
