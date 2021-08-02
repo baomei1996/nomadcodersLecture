47b4e0a9985907cdd80f578ff355e72e1b3977b0
BomiAn <qhal4686@gmail.com>
2021-08-02 23:59:24 +0900

    vanilaJS

--- vanilaJS/js/weather.js
+++ vanilaJS/js/weather.js
@@ -1,7 +1,11 @@
-function onGeoOk (position) {
-  console.log(position)
-}
+const API_KEY = "0439b9206862f08316c09c6391613aef";
 
+function onGeoOk (position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("you live in ", lat, lng)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+}
 function onGeoError () {
   alert("Can't find you. No weather for you.")
 }
