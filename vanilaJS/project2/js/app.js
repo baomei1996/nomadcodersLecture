20429a83e2f4c315e530f58de79a049e6d70c454
BomiAn <qhal4686@gmail.com>
2021-08-07 23:58:44 +0900

    nomadcodersLecture - vanilaJS

--- vanilaJS/project2/js/app.js
+++ vanilaJS/project2/js/app.js
@@ -1,10 +1,20 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const color = document.getElementsByClassName('jsColor')
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
-ctx.strokdStyle = '#2c2c2c'
+const INITIAL_COLOR ="2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+ctx.strokeStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
+ctx.fillStyle =  INITIAL_COLOR;
 
 let painting = false;
+let filling = false;
 
 function stopPainting () {
   painting = false;
@@ -17,7 +27,19 @@ function startPainting () {
 function onMouseMove (event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y)
+  if(!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y)
+    ctx.stroke();
+  }
+}
+
+function handleCanvasClick () {
+  if(filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+  }
 }
 
 if(canvas) {
@@ -25,4 +47,36 @@ if(canvas) {
   canvas.addEventListener('mousedown', startPainting)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
+  canvas.addEventListener('click', handleCanvasClick)
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+Array.from(color).forEach(color => color.addEventListener('click', handleColorClick))
+
+function handleRangeChange (event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick (event) {
+  if(filling == true){
+    filling = false;
+    mode.innerText = 'FIll'
+  } else {
+    filling = true;
+    mode.innerText = 'Paint'
+  }
+} 
+
+if(range) {
+  range.addEventListener('input', handleRangeChange)
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick)
 }
