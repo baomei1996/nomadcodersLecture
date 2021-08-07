caadf61c19de81fa6157d11c0f1e5591b988702c
BomiAn <qhal4686@gmail.com>
2021-08-08 00:43:17 +0900

    namadcdersLecture - vanilaJS

--- vanilaJS/project2/js/app.js
+++ vanilaJS/project2/js/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const color = document.getElementsByClassName('jsColor')
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const INITIAL_COLOR ="2c2c2c";
 const CANVAS_SIZE = 700;
@@ -11,7 +12,10 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 ctx.strokeStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
-ctx.fillStyle =  INITIAL_COLOR;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+
+
 
 let painting = false;
 let filling = false;
@@ -42,12 +46,17 @@ function handleCanvasClick () {
   }
 }
 
+function handleCanvasCM (event) {
+  event.preventDefault();
+}
+
 if(canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPainting)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
   canvas.addEventListener('click', handleCanvasClick)
+  canvas.addEventListener('contextmenu', handleCanvasCM)
 }
 
 function handleColorClick(event) {
@@ -73,10 +82,23 @@ function handleModeClick (event) {
   }
 } 
 
+function handleSaveClick () {
+  const image = canvas.toDataURL()
+  const link = document.createElement('a')
+  link.href = image
+  link.download = ' paintJS[🎨]'
+  link.click();
+
+}
+
 if(range) {
   range.addEventListener('input', handleRangeChange)
 }
 
 if (mode) {
   mode.addEventListener('click', handleModeClick)
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', handleSaveClick)
 }
